fcd6fc6b564108138d0acc4f7a68569b957e96b5
MargoSamo <margo_ii@mail.ru>
2022-10-22 17:33:39 +0400

    differences junit5 from junit4

--- src/test/java/com/mariia/calc/Vector2DTest.java
+++ src/test/java/com/mariia/calc/Vector2DTest.java
@@ -1,29 +1,31 @@
 package com.mariia.calc;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class Vector2DTest {
 
+    private final double EPS = 1e-9;
+    private static Vector2D v1;
+
+    @BeforeAll
+    public static void createNewVector() {
+        v1 = new Vector2D();
+    }
+
     @Test
     public void newVectorShouldHaveZeroLength() {
-        Vector2D v1 = new Vector2D(); //action (deistvie)
-
-        // assertion (proverit cto ojidaemiy rezultat covpadaet c facticeckim)
-        Assert.assertEquals(0, v1.length(), 1e-9);
+        Assert.assertEquals(0, v1.length(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroX() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getX(), 1e-9);
+        Assert.assertEquals(0, v1.getX(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroY() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getY(), 1e-9);
+        Assert.assertEquals(0, v1.getY(), EPS);
     }
 }
